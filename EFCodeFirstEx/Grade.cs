72309f01686f333a5154b076ff2d9e7955074622
danushka <danushka@ecyber.com>
2018-06-26 15:15:22 +0530

    model classes created

---
+++ EFCodeFirstEx/Grade.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace EFCodeFirstEx
+{
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
--- EFCodeFirstEx/Program.cs
+++ EFCodeFirstEx/Program.cs
@@ -10,6 +10,16 @@ namespace EFCodeFirstEx
     {
         static void Main(string[] args)
         {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+                Console.WriteLine("Finished ");
+
+                Student s = ctx.Students.First();
+                Console.WriteLine(s.StudentName);
+            }
         }
     }
 }
