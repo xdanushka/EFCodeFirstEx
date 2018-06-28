34017197ef69e36f0b355e7d74f129eaa16acee6
danushka <danushka@ecyber.com>
2018-06-28 16:00:36 +0530

    fluent api

--- EFCodeFirstEx/SchoolContext.cs
+++ EFCodeFirstEx/SchoolContext.cs
@@ -17,6 +17,82 @@ namespace EFCodeFirstEx
         public DbSet<Student> Students { get; set; }
         public DbSet<Grade> Grades { get; set; }
 
-        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            //configure default schema
+            modelBuilder.HasDefaultSchema("Admin");
+
+            //map entity to table
+            //    modelBuilder.Entity<Student>().ToTable("StudentInfo");
+            //    modelBuilder.Entity<Standard>().ToTable("StandardInfo", "dbo");
+
+            //mapping entity properties to multiple tables
+            modelBuilder.Entity<Student>().Map(m =>
+            {
+                m.Properties(p => new
+                {
+                    p.StudentId,
+                    p.StudentName
+                });
+                m.ToTable("StudentInfo");
+            }).Map(m =>
+            {
+                m.Properties(p => new
+                {
+                    p.StudentId,
+                    p.Height,
+                    p.Weight,
+                    p.Photo,
+                    p.DateOfBirth
+                });
+                m.ToTable("StudentInfoDetail");
+            });
+
+            modelBuilder.Entity<Standard>().ToTable("StandardInfo");
+
+            //configuring primary key
+            modelBuilder.Entity<Student>().HasKey(s => s.StudentId);
+            modelBuilder.Entity<Standard>().HasKey(s => s.StandardId);
+
+            //configure composite primary key
+            modelBuilder.Entity<Student>().HasKey(s => new
+            {
+                s.StudentId,
+                s.StudentName
+            });
+
+            //configure column
+            modelBuilder.Entity<Student>()
+                .Property(p => p.DateOfBirth)
+                .HasColumnName("DoB")
+                .HasColumnOrder(3)
+                .HasColumnType("dateTime2");
+
+            //configure null column
+            modelBuilder.Entity<Student>()
+                .Property(p => p.Height)
+                .IsOptional();
+
+            //configuring notnull columns
+            modelBuilder.Entity<Student>()
+                .Property(p => p.Weight)
+                .IsRequired();
+
+            //set column size
+            modelBuilder.Entity<Student>()
+                .Property(p => p.StudentName)
+                .HasMaxLength(50);
+
+            //change data type from nvarchar to char
+            modelBuilder.Entity<Student>()
+                .Property(p => p.Height)
+                .HasPrecision(2, 2);
+
+            //set a concurrency column
+            modelBuilder.Entity<Student>()
+                .Property(p => p.StudentName)
+                .IsConcurrencyToken();
+        }
     }
 }
--- EFCodeFirstEx/Student.cs
+++ EFCodeFirstEx/Student.cs
@@ -52,6 +52,8 @@ namespace EFCodeFirstEx
         [Index("INDEX_REGNUM", IsClustered = true, IsUnique = true)] 
         public int RegistrationNumber { get; set; }
 
+
+        public virtual StudentAddress Address { get; set; }
     }
 
     public class Standard
@@ -61,5 +63,18 @@ namespace EFCodeFirstEx
 
         public ICollection<Student> Students { get; set; }
 
+    }
+
+    public class StudentAddress
+    {
+        public int StudentAddressId { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public int Zipcode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+        public virtual Student Student { get; set; }
     }
 }
